dd73ab7b9a856b0ffd1ad32938c6f281dd122e3d
R_Kirlew <kirlewraheem@gmail.com>
2023-04-13 20:37:45 -0700

    Update main.cpp
    Added inverse calculation and identity matrix generator

--- main.cpp
+++ main.cpp
@@ -9,6 +9,92 @@ private:
     std::vector<std::vector<double>>data;
 public:
     Matrix(int rows, int cols):rows(rows),cols(cols),data(rows,std::vector<double>(cols,0.0)){}
+    int numRows() const {
+        return rows;
+    }
+
+    int numCols() const {
+        return cols;
+    }
+    static Matrix generateIdentity(int size) {
+        Matrix identity(size, size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) {
+                    identity.set(i, j, 1.0); // Set diagonal elements to 1
+                }
+                else {
+                    identity.set(i, j, 0.0); // Set non-diagonal elements to 0
+                }
+            }
+        }
+        return identity;
+    }
+
+    // Function to calculate the inverse of a matrix
+
+    Matrix inverse() const {
+        if (rows != cols) {
+            // Matrix is not square, so it does not have an inverse
+            throw std::runtime_error("Matrix is not square, so it does not have an inverse");
+        }
+
+        int size = rows;
+
+        // Create an identity matrix of the same size as the original matrix
+        Matrix identity = generateIdentity(size);
+
+        // Create a copy of the original matrix to avoid modifying the original matrix
+        Matrix copy(*this);
+
+        // Perform Gaussian elimination to transform the original matrix into the identity matrix,
+        // while applying the same row operations to the identity matrix
+        for (int i = 0; i < size; i++) {
+            // Find the pivot element
+            int maxRowIndex = i;
+            double maxRowValue = std::abs(copy.get(i, i));
+            for (int j = i + 1; j < size; j++) {
+                double absValue = std::abs(copy.get(j, i));
+                if (absValue > maxRowValue) {
+                    maxRowIndex = j;
+                    maxRowValue = absValue;
+                }
+            }
+
+            // Swap rows to bring the pivot element to the diagonal
+            if (maxRowIndex != i) {
+                copy.swapRows(i, maxRowIndex);
+                identity.swapRows(i, maxRowIndex);
+            }
+
+            // Scale the row so that the pivot element becomes 1
+            double pivot = copy.get(i, i);
+            for (int j = 0; j < size; j++) {
+                copy.set(i, j, copy.get(i, j) / pivot);
+                identity.set(i, j, identity.get(i, j) / pivot);
+            }
+
+            // Perform row operations to make all other elements in the same column as the pivot element become 0
+            for (int j = 0; j < size; j++) {
+                if (j != i) {
+                    double factor = copy.get(j, i);
+                    for (int k = 0; k < size; k++) {
+                        copy.set(j, k, copy.get(j, k) - factor * copy.get(i, k));
+                        identity.set(j, k, identity.get(j, k) - factor * identity.get(i, k));
+                    }
+                }
+            }
+        }
+
+        return identity;
+    }
+    void swapRows(int row1, int row2) {
+        for (int j = 0; j < cols; j++) {
+            double temp = data[row1][j];
+            data[row1][j] = data[row2][j];
+            data[row2][j] = temp;
+        }
+    }
     void set(int row, int col, double value) {
         data[row][col] = value;
     }
@@ -132,7 +218,10 @@ int main()
   
     // Calculate determinant
     double det = matrix1.determinant();
-
+    Matrix inverseMatrix = matrix1.inverse();
+    std::cout << "Matrix 1 Inverse:" << std::endl;
+    inverseMatrix.print();
+   
     // Print the result
     std::cout << "Matrix 1:" << std::endl;
     matrix1.print();
